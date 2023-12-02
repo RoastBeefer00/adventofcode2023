a7c4831cdb31e1f71f6fb1c9f73985cc30c7ff66
Jake Jasmin <roastbeefer000@gmail.com>
2023-12-02 15:38:34 -0700

    day one part one done

--- day1/src/bin/part2.rs
+++ day1/src/bin/part2.rs
@@ -1,14 +1,15 @@
 use anyhow::Result;
+use regex::Regex;
 use std::fs;
 
 #[derive(Clone)]
 struct Number {
-    name: &'static str,
-    value: &'static str,
+    name: String,
+    value: String,
 }
 
 impl Number {
-    fn new(name: &'static str, value: &'static str) -> Self {
+    fn new(name: String, value: String) -> Self {
         Number { name, value }
     }
 }
@@ -18,8 +19,6 @@ fn main() -> Result<()> {
     let lines: Vec<String> = input.lines().map(String::from).collect();
     let parsed_input = parse_input(lines.clone());
 
-    println!("{:#?}", lines.clone().get(0));
-    println!("{:#?}", parsed_input.clone().get(0));
     let digit_lines: Vec<String> = parsed_input
         .into_iter()
         .map(|line| {
@@ -30,46 +29,50 @@ fn main() -> Result<()> {
         })
         .collect();
 
-    // println!("{:#?}", digit_lines.clone().);
     let sum = add_lines(digit_lines)?;
     println!("{:#?}", sum);
     Ok(())
 }
 
 fn parse_input(input: Vec<String>) -> Vec<String> {
-    let parsed_strings: Vec<String> = input;
-    let letters = vec![
-        "e",
-        "f",
-        "n",
-        "o",
-        "s",
-        "t",
-    ]
+    let re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").unwrap();
+
     let numbers: Vec<Number> = vec![
-        Number::new("one", "1"),
-        Number::new("two", "2"),
-        Number::new("three", "3"),
-        Number::new("four", "4"),
-        Number::new("five", "5"),
-        Number::new("six", "6"),
-        Number::new("seven", "7"),
-        Number::new("eight", "8"),
-        Number::new("nine", "9"),
+        Number::new("one".to_string(), "1".to_string()),
+        Number::new("two".to_string(), "2".to_string()),
+        Number::new("three".to_string(), "3".to_string()),
+        Number::new("four".to_string(), "4".to_string()),
+        Number::new("five".to_string(), "5".to_string()),
+        Number::new("six".to_string(), "6".to_string()),
+        Number::new("seven".to_string(), "7".to_string()),
+        Number::new("eight".to_string(), "8".to_string()),
+        Number::new("nine".to_string(), "9".to_string()),
     ];
 
-    parsed_strings
+    input
         .into_iter()
         .map(|line| {
-            let mut parsed_string = line;
-
-            for number in numbers.clone() {
-                if parsed_string.contains(number.name) {
-                    parsed_string = parsed_string.replace(number.name, number.value);
-                }
+            // println!("Line before: {}", line);
+            let mut matches = Vec::new();
+            let mut matches_string = String::new();
+            for cap in re.captures_iter(&line) {
+                matches.push(cap.get(0).unwrap().as_str().to_string());
             }
+            if matches.is_empty() {
+                panic!("This line is empty: {:#?}", line);
+            }
+            matches.iter_mut().for_each(|mat| {
+                for number in numbers.clone() {
+                    if mat == &number.name {
+                        *mat = number.value;
+                    }
+                }
+                matches_string += mat;
+            });
+            // println!("{:#?}", matches);
+            // println!("Line after: {}", matches_string.clone());
 
-            parsed_string
+            matches_string
         })
         .collect::<Vec<_>>()
 }
@@ -78,11 +81,17 @@ fn add_lines(lines: Vec<String>) -> Result<i32> {
     let digits: Vec<i32> = lines
         .into_iter()
         .map(|line| {
+            println!("Line before: {}", line);
+            if line == "" {
+                println!("This line is empty: {:#?}", line);
+                return 0;
+            }
             let nums = format!(
                 "{}{}",
                 line.chars().nth(0).unwrap(),
                 line.chars().nth(line.len() - 1).unwrap()
             );
+            println!("Line before: {}", nums.clone());
             nums.parse::<i32>().unwrap()
         })
         .collect();
