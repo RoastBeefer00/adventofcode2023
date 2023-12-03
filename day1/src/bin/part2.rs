8cd4a9f514f7bce4ff2492546c715434a8b2adfe
Jake Jasmin <roastbeefer000@gmail.com>
2023-12-02 18:13:42 -0700

    fix day2

--- day1/src/bin/part2.rs
+++ day1/src/bin/part2.rs
@@ -19,60 +19,62 @@ fn main() -> Result<()> {
     let lines: Vec<String> = input.lines().map(String::from).collect();
     let parsed_input = parse_input(lines.clone());
 
-    let digit_lines: Vec<String> = parsed_input
-        .into_iter()
-        .map(|line| {
-            line.chars()
-                .into_iter()
-                .filter(|char| char.is_numeric())
-                .collect()
-        })
-        .collect();
+    println!("{:#?}", parsed_input);
+    // let digit_lines: Vec<String> = parsed_input
+    //     .into_iter()
+    //     .map(|line| {
+    //         line.chars()
+    //             .into_iter()
+    //             .filter(|char| char.is_numeric())
+    //             .collect()
+    //     })
+    //     .collect();
 
-    let sum = add_lines(digit_lines)?;
+    let sum = add_lines(parsed_input)?;
     println!("{:#?}", sum);
     Ok(())
 }
 
 fn parse_input(input: Vec<String>) -> Vec<String> {
-    let re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").unwrap();
-
-    let numbers: Vec<Number> = vec![
-        Number::new("one".to_string(), "1".to_string()),
-        Number::new("two".to_string(), "2".to_string()),
-        Number::new("three".to_string(), "3".to_string()),
-        Number::new("four".to_string(), "4".to_string()),
-        Number::new("five".to_string(), "5".to_string()),
-        Number::new("six".to_string(), "6".to_string()),
-        Number::new("seven".to_string(), "7".to_string()),
-        Number::new("eight".to_string(), "8".to_string()),
-        Number::new("nine".to_string(), "9".to_string()),
-    ];
-
     input
         .into_iter()
         .map(|line| {
             // println!("Line before: {}", line);
-            let mut matches = Vec::new();
-            let mut matches_string = String::new();
-            for cap in re.captures_iter(&line) {
-                matches.push(cap.get(0).unwrap().as_str().to_string());
-            }
-            if matches.is_empty() {
-                panic!("This line is empty: {:#?}", line);
-            }
-            matches.iter_mut().for_each(|mat| {
-                for number in numbers.clone() {
-                    if mat == &number.name {
-                        *mat = number.value;
-                    }
-                }
-                matches_string += mat;
-            });
-            // println!("{:#?}", matches);
-            // println!("Line after: {}", matches_string.clone());
+            let mut index = 0;
+            let line_iter = std::iter::from_fn(move || {
+                let slice = &line[index..];
+                let result = if slice.starts_with("one") {
+                    Some('1')
+                } else if slice.starts_with("two") {
+                    Some('2')
+                } else if slice.starts_with("three") {
+                    Some('3')
+                } else if slice.starts_with("four") {
+                    Some('4')
+                } else if slice.starts_with("five") {
+                    Some('5')
+                } else if slice.starts_with("six") {
+                    Some('6')
+                } else if slice.starts_with("seven") {
+                    Some('7')
+                } else if slice.starts_with("eight") {
+                    Some('8')
+                } else if slice.starts_with("nine") {
+                    Some('9')
+                } else {
+                    slice.chars().next()
+                };
+                index += 1;
 
-            matches_string
+                result
+            });
+
+            let mut it = line_iter.filter_map(|character| character.to_digit(10));
+            let first = it.next().expect("should be a number");
+            match it.last() {
+                Some(value) => format!("{first}{value}"),
+                None => format!("{first}{first}"),
+            }
         })
         .collect::<Vec<_>>()
 }
@@ -81,18 +83,18 @@ fn add_lines(lines: Vec<String>) -> Result<i32> {
     let digits: Vec<i32> = lines
         .into_iter()
         .map(|line| {
-            println!("Line before: {}", line);
-            if line == "" {
-                println!("This line is empty: {:#?}", line);
-                return 0;
-            }
-            let nums = format!(
-                "{}{}",
-                line.chars().nth(0).unwrap(),
-                line.chars().nth(line.len() - 1).unwrap()
-            );
-            println!("Line before: {}", nums.clone());
-            nums.parse::<i32>().unwrap()
+            // println!("Line before: {}", line);
+            // if line == "" {
+            //     println!("This line is empty: {:#?}", line);
+            //     return 0;
+            // }
+            // let nums = format!(
+            //     "{}{}",
+            //     line.chars().nth(0).unwrap(),
+            //     line.chars().nth(line.len() - 1).unwrap()
+            // );
+            // println!("Line before: {}", nums.clone());
+            line.parse::<i32>().unwrap()
         })
         .collect();
     Ok(digits.iter().sum())
