8abeafb523c51b54451dd8736ec41a32b7f9b86e
Jake Jasmin <roastbeefer000@gmail.com>
2023-12-11 18:08:11 -0700

    day6 complete

--- day6/src/bin/part2.rs
+++ day6/src/bin/part2.rs
@@ -11,25 +11,19 @@ use std::fs;
 use std::iter::zip;
 use std::ops::Range;
 
-fn parse_times(input: &str) -> IResult<&str, Vec<u32>> {
-    let (input, values) = tag("Time: ")
-        .precedes(separated_list1(space1, complete::u32.map(|num| num)).preceded_by(multispace0))
-        .parse(input)?;
-
-    Ok((input, values))
+fn parse_times(input: &str) -> Result<u64> {
+    let new_input: String = input.chars().filter(|c| c.is_ascii_digit()).collect();
+    Ok(new_input.parse::<u64>()?)
 }
 
-fn parse_distances(input: &str) -> IResult<&str, Vec<u32>> {
-    let (input, values) = tag("Distance: ")
-        .precedes(separated_list1(space1, complete::u32.map(|num| num)).preceded_by(multispace0))
-        .parse(input)?;
-
-    Ok((input, values))
+fn parse_distances(input: &str) -> Result<u64> {
+    let new_input: String = input.chars().filter(|c| c.is_ascii_digit()).collect();
+    Ok(new_input.parse::<u64>()?)
 }
 
-fn process_race(time: u32, distance: u32) -> Result<u32> {
+fn process_race(time: u64, distance: u64) -> Result<u64> {
     let range = 0..time;
-    let distances: Vec<u32> = range
+    let distances: Vec<u64> = range
         .into_iter()
         .map(|seconds| seconds * (time - seconds))
         .collect();
@@ -38,24 +32,18 @@ fn process_race(time: u32, distance: u32) -> Result<u32> {
         .into_iter()
         .filter(|&x| x > distance)
         .collect::<Vec<_>>()
-        .len() as u32)
+        .len() as u64)
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("input.txt").unwrap();
     let mut lines = input.lines();
 
-    let (_, races) = parse_times(&lines.next().unwrap()).unwrap();
-    let (_, distances) = parse_distances(&lines.last().unwrap()).unwrap();
+    let time = parse_times(&lines.next().unwrap()).unwrap();
+    let distance = parse_distances(&lines.last().unwrap()).unwrap();
 
-    let races: Vec<(u32, u32)> = zip(races, distances).collect();
-    let wins: Vec<u32> = races
-        .into_iter()
-        .map(|(time, distance)| process_race(time, distance).unwrap())
-        .collect();
-
-    let result = wins.into_iter().fold(1, |acc, win| acc * win);
-    println!("{result}");
+    let wins = process_race(time, distance).unwrap();
+    println!("{wins}");
     Ok(())
 }
 
@@ -69,17 +57,12 @@ mod tests {
         let input = fs::read_to_string("test.txt").unwrap();
 
         let mut lines = input.lines();
-        let (_, races) = parse_times(&lines.next().unwrap()).unwrap();
-        let (_, distances) = parse_distances(&lines.last().unwrap()).unwrap();
+        let time = parse_times(&lines.next().unwrap()).unwrap();
+        let distance = parse_distances(&lines.last().unwrap()).unwrap();
 
-        let races: Vec<(u32, u32)> = zip(races, distances).collect();
-        let wins: Vec<u32> = races
-            .into_iter()
-            .map(|(time, distance)| process_race(time, distance).unwrap())
-            .collect();
+        let wins = process_race(time, distance).unwrap();
 
-        let result = wins.into_iter().fold(1, |acc, win| acc * win);
-        assert_eq!(288, result);
+        assert_eq!(71503, wins);
         Ok(())
     }
 }
